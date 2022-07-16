0ac8f3b96b0bcc75aed76088d29dfde580872fb8
Lucas <luan237@gmail.com>
2022-07-16 17:31:23 -0400

    completed add new employee

--- server/routes/addnew.js
+++ server/routes/addnew.js
@@ -1,64 +1,83 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const uuid = require("uuid");
 // BCRYPT//
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 ///
 const knex = require("knex")(require("../knexfile"));
 //
-router.route("/").post((req, res) => {
-  const data = req.body;
-  const employeeInfo = {
-    id: data.id,
-    position: data.position,
-    name: data.name,
-    wage: data.wage,
-    avatar: "/avatar/avatar(1).jpg",
-    phone: data.phone,
-    address: data.address,
-  };
-  knex("employee_list")
-    .select("id", "name")
-    .where({ id: data.id })
-    .orWhere({ name: data.name })
-    .then((data1) => {
-      if (data1.length != 0) {
-        return res
-          .status(400)
-          .send(`Duplicated with id: ${data1[0].id}, name: ${data1[0].name}`);
-      } else {
-        knex("employee_list")
-          .insert(employeeInfo)
-          .then((data2) => {
-            const encryptPassword = async () => {
-              try {
-                await bcrypt.hash(data.password, saltRounds, (err, hash) => {
-                  let loginInfo = {
-                    id: data.id,
-                    position: data.position,
-                    password: hash,
-                  };
-                  knex("login")
-                    .insert(loginInfo)
-                    .catch((err) => {
-                      console.log("login table error: ", err);
-                      return res.status(400).send("An error has occured");
-                    })
-                    .then((res) => {});
-                  return res.json("Success");
-                });
-              } catch (err) {
-                console.log("Error when encrypting: ", err);
-              }
-            };
-            encryptPassword();
-          })
-          .catch((err) => {
-            console.log("employee_list error: ", err);
-            return res.status(401).send("An error has occured");
-          });
-      }
-    });
+const newImageName = `avatar-${uuid.v4()}`;
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./data/avatar/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, newImageName + file.originalname);
+  },
 });
+router
+  .route("/")
+  .post(multer({ storage: storage }).single("avatar"), (req, res) => {
+    let imageName;
+    if (req.file) {
+      imageName = "/avatar/" + newImageName + req.file.originalname;
+    } else {
+      imageName = "/avatar/no-avatar.png";
+    }
+    const data = req.body;
+    const employeeInfo = {
+      id: data.id,
+      position: data.position,
+      name: data.name,
+      wage: data.wage,
+      avatar: imageName,
+      phone: data.phone,
+      address: data.address,
+    };
+    knex("employee_list")
+      .select("id", "name")
+      .where({ id: data.id })
+      .orWhere({ name: data.name })
+      .then((data1) => {
+        if (data1.length != 0) {
+          return res
+            .status(400)
+            .send(`Duplicated with id: ${data1[0].id}, name: ${data1[0].name}`);
+        } else {
+          knex("employee_list")
+            .insert(employeeInfo)
+            .then((data2) => {
+              const encryptPassword = async () => {
+                try {
+                  await bcrypt.hash(data.password, saltRounds, (err, hash) => {
+                    let loginInfo = {
+                      id: data.id,
+                      position: data.position,
+                      password: hash,
+                    };
+                    knex("login")
+                      .insert(loginInfo)
+                      .catch((err) => {
+                        console.log("login table error: ", err);
+                        return res.status(400).send("An error has occured");
+                      })
+                      .then((res) => {});
+                    return res.json("Successfully added new Employee");
+                  });
+                } catch (err) {
+                  console.log("Error when encrypting: ", err);
+                }
+              };
+              encryptPassword();
+            })
+            .catch((err) => {
+              console.log("employee_list error: ", err);
+              return res.status(401).send("An error has occured");
+            });
+        }
+      });
+  });
 
 module.exports = router;
--- server/routes/misc.js
+++ server/routes/misc.js
@@ -9,18 +9,34 @@ const fetchUser = async () => {
   return getData;
 };
 
-router.route("/:id").get((req, res) => {
-  const id = req.params.id;
-  let userList = fetchUser()
-    .then((data) => (userList = data))
-    .then(() => {
-      const found = userList.find((user) => user.id == id);
-      if (found) {
-        return res.status(200).send(found.avatar);
-      } else {
-        return res.status(400).send(null);
-      }
-    });
-});
+router
+  .route("/:id")
+  .get((req, res) => {
+    knex("employee_list")
+      .select("avatar")
+      .where({ id: req.params.id })
+      .then((data) => {
+        if (data.length === 0) {
+          return res.status(200).send("/avatar/no-avatar.png");
+        }
+        return res.status(200).send(data[0].avatar);
+      });
+  })
+  .delete((req, res) => {
+    knex("employee_list")
+      .del()
+      .where({ id: req.params.id })
+      .then((data) => {
+        if (data === 0) {
+          return res
+            .status(400)
+            .json(`Can't find employee with id ${req.params.id}`);
+        } else if (data === 1) {
+          return res
+            .status(200)
+            .json(`Deleted employee number ${req.params.id}`);
+        }
+      });
+  });
 
 module.exports = router;
